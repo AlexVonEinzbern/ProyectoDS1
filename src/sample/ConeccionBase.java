4e6e77e60d96344cb914a68f668437ad61227cef
Jose Fernando Botina <joseferbt@gmail.com>
2020-10-26 11:42:11 -0500

    Avance en clases
    Creacion de clases auxiliares

---
+++ src/sample/ConeccionBase.java
@@ -0,0 +1,38 @@
+package sample;
+import java.sql.*;
+
+public class ConeccionBase  {
+private Connection base=null;
+public ConeccionBase(){iniciarC();}
+
+    private void iniciarC(){
+        String urlDatabase =  "jdbc:postgresql://localhost:5432/postgres";
+        try {
+            Class.forName("org.postgresql.Driver");
+            this.base = DriverManager.getConnection(urlDatabase,  "postgres", "1830628");
+
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error : "+e.getMessage());
+        }
+        System.out.println("Conexión Exitosa");
+    }
+    public Connection getConect() {
+        return base;
+    }
+
+    public String consulta() throws SQLException {
+
+        PreparedStatement stmt = this.base.prepareStatement("SELECT nombref FROM fabricante where codigof = 1");
+        ResultSet r = stmt.executeQuery();
+        String aux = "";
+        if (r.next()) {
+            aux=  r.getString("nombref");
+            System.out.println(aux + " este es el resutado");
+        }
+        r.close();
+        stmt.close();
+        return aux;
+    }
+
+}
+
---
+++ src/sample/Ventana.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Connection;
+
+public class Ventana extends Stage {
+
+    private Connection base;
+    public Ventana(Connection base,String fxmlrute,String title) throws IOException {
+        this.base=base;
+        Parent root = FXMLLoader.load(getClass().getResource(fxmlrute));
+        Scene scene = new Scene(root,500,500);
+        this.setScene(scene);
+        this.setTitle(title);
+        this.show();
+    }
+}
