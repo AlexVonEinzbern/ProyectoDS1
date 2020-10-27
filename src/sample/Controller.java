79dea1d9e001d7f46d200b197ef2074fa751e7eb
andres-saa <andrew19f@gmail.com>
2020-10-27 18:22:15 -0500

    interaccion en los botenes superiores

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,40 @@
 package sample;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+
 public class Controller {
+
+    @FXML private ImageView loginArrow;
+    @FXML private ImageView helpArrow;
+    @FXML private AnchorPane loginPanel;
+    @FXML private AnchorPane helpPanel;
+
+    public void botonExit(MouseEvent event){
+        Platform.exit();
+        System.exit(0);
+    }
+
+    public void botonLogin(MouseEvent event){
+
+        loginPanel.setVisible(true);
+        helpPanel.setVisible(false);
+        loginArrow.setVisible(true);
+        helpArrow.setVisible(false);
+    }
+
+    public void BotonHelp(MouseEvent event){
+
+        loginPanel.setVisible(false);
+        helpPanel.setVisible(true);
+        loginArrow.setVisible(false);
+        helpArrow.setVisible(true);
+
+    }
+
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,27 +1,53 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import modelo.Conexion;
+import org.jetbrains.annotations.NotNull;
 
 public class Main extends Application {
-
+    private double xoffset;
+    private double yoffset;
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(@NotNull Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+
+        //esto es para que la interfaz se pueda mover con el mouse
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xoffset=event.getSceneX();
+                yoffset=event.getSceneY();
+            }
+        });
+
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                primaryStage.setX(event.getScreenX()-xoffset);
+                primaryStage.setY(event.getScreenY()-yoffset);
+            }
+        });
+
+
         primaryStage.initStyle(StageStyle.TRANSPARENT);
-        primaryStage.setTitle("Hello World");
         Scene scene=new Scene(root);
         scene.setFill(Color.TRANSPARENT);
         primaryStage.setScene(scene);
+        primaryStage.setTitle("login");
         primaryStage.show();
 
         Conexion con = new Conexion();
+
     }
 
 
