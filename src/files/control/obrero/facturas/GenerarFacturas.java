681c7c2b7b0f65de41e95e04336bb1f3afe1d6af
Jose Fernando Botina <joseferbt@gmail.com>
2020-11-25 23:47:27 -0500

    registrar pago (buscar)

--- src/files/control/obrero/facturas/GenerarFacturas.java
+++ src/files/control/obrero/facturas/GenerarFacturas.java
@@ -25,18 +25,66 @@ public class GenerarFacturas {
     @FXML    private Button registrar;
     @FXML    private Button imprimir;
     @FXML    private Button limpiar;
+    @FXML    private TextField idUsuario;
+
 
     @FXML    void BuscarFactura(ActionEvent event) throws SQLException {
-        ResultSet rs = con.consultar("select ");
+        int idFac= Integer.parseInt(idFactura.getText());
+        int valor;
+        ResultSet rs = con.consultar(
+                "select c.idCliente,nombreCliente,cedulaCliente,estadoCliente,medida,fechaVenceFactura," +
+                        "reconexion,unidadEnergia, fechaVenceFactura - current_date as interes"+
+                        "from factura as f,cliente as c,detalleFactura as d,medida as m,configurarsistema as con" +
+                        " where f.idfactura = "+idFac+" and f.idCliente = c.idCliente and " +
+                        "f.idfactura = d.idfactura and c.idcliente = m.idcliente " +
+                        "and d.idConfiguracion = con.idConfiguracion");
+        if(rs.next()){
+            idCliente.setText(""+rs.getInt(1));
+            nombreCliente.setText(rs.getString(2));
+            cedulaCLiente.setText(rs.getString(3));
+            fechaDeVencimiento.setText(String.valueOf(rs.getDate(6)));
+            valor = rs.getInt(5)*rs.getInt(8);
+            int interes = rs.getInt(9);
+            if(interes<0){
+                if((interes*-1)>30){
+                    valor *= 0.30;
+                }else{
+                    valor *= (interes*-1/100);
+                }
+            }
+            if(!rs.getBoolean(4)){
+                valor+=  rs.getInt("7");
+            }
+
+            valorApagar.setText(""+valor);
+        }
     }
-
-    @FXML    void registrarPago(ActionEvent event) {
-
+// la ultima configuracion del sistema es (select * from configurarsistema order by idconfiguracion desc limit 1)
+    @FXML    void registrarPago(ActionEvent event) throws SQLException {
+        if(isFill()) {
+            int i = con.guardar("insert into RegistroPago (idUsuario,idFactura,pago,fechaPago)" +
+                    "values ()");
+        }
+        clear();
     }
     @FXML    void limpiarCampos(ActionEvent event) {
-
+        clear();
     }
     @FXML    void imprimirRecibo(ActionEvent event) {
-
+        clear();
+    }
+    private Boolean isFill(){
+        return (!(idFactura.getText().equals("")) &&
+                !(idCliente.getText().equals(""))&&
+               !(idUsuario.getText().equals("")) );
+    }
+    private void clear(){
+        idCliente.clear();
+        idFactura.clear();
+        nombreCliente.clear();
+        idUsuario.clear();
+        cedulaCLiente.clear();
+        fechaDeVencimiento.clear();
+        valorApagar.clear();
     }
 }
