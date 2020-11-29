bbc8f09603a68c9275e33f3c87186f752de2ed84
Jose Fernando Botina <joseferbt@gmail.com>
2020-11-29 11:11:01 -0500

    EnviarFacturas funcional

---
+++ src/files/modelo/EnviarFacturas.java
@@ -0,0 +1,57 @@
+package files.modelo;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.util.Properties;
+
+public class EnviarFacturas {
+
+    public static void enviarConGMail(String destinatario, String asunto, String cuerpo,String idcliente,String nombreCliente) {
+        // Esto es lo que va delante de @gmail.com en tu cuenta de correo. Es el remitente también.
+        String remitente = "energylmtd@gmail.com";  //Para la dirección nomcuenta@gmail.com
+
+        Properties props = System.getProperties();
+        props.put("mail.smtp.host", "smtp.gmail.com");  //El servidor SMTP de Google
+        props.put("mail.smtp.user", remitente);
+        props.put("mail.smtp.clave", "password1234.");    //La clave de la cuenta
+        props.put("mail.smtp.auth", "true");    //Usar autenticación mediante usuario y clave
+        props.put("mail.smtp.starttls.enable", "true"); //Para conectar de manera segura al servidor SMTP
+        props.put("mail.smtp.port", "587"); //El puerto SMTP seguro de Google
+
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+
+        try {
+            message.setFrom(new InternetAddress(remitente));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(destinatario));   //Se podrían añadir varios de la misma manera
+            message.setSubject(asunto);
+            message.setText(cuerpo);
+
+            MimeBodyPart messageBodyPart = new MimeBodyPart();
+
+            Multipart multipart = new MimeMultipart();
+
+            String file = "src/files/facturasClientes/"+idcliente+".pdf";
+            String fileName = "Factura "+nombreCliente;
+            DataSource source = new FileDataSource(file);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName(fileName);
+            multipart.addBodyPart(messageBodyPart);
+            message.setContent(multipart);
+
+            Transport transport = session.getTransport("smtp");
+            transport.connect("smtp.gmail.com", remitente, "password1234.");
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        }
+        catch (MessagingException me) {
+            me.printStackTrace();   //Si se produce un error
+        }
+    }
+}
